872328bc3623116f13247d832d09fdb4f78d3fca
33Zonkit2019 <you@example.com>
2023-10-27 18:56:38 +0500

    homework

---
+++ hmeworke/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <p class="square1"></p>
+    <p class="square2"></p>
+    <p class="square3"></p>
+    <p class="square4"></p>
+    <p class="square5"></p>
+    <p class="square6"></p>
+    <Lol/>
+  </div>
+</template>
+<script>
+import Lol from './components/lol.vue';
+export default {
+    components: { Lol }
+}
+</script>
+<style>
+  .square1{
+    width: 40px;
+    height: 40px;
+    background-color: red;
+
+  }
+  .square2{
+    width: 50px;
+    height: 50px;
+    background-color: green;
+  }
+  .square3{
+    width: 60px;
+    height: 60px;
+    background-color: blue;
+  }
+  .square4{
+    width: 70px;
+    height: 70px;
+    background-color: orange;
+
+  }
+  .square5{
+    width: 80px;
+    height: 80px;
+    background-color: purple;
+
+  }
+  .square6{
+    width: 90px;
+    height: 90px;
+    background-color: black;
+
+  }
+</style>
---
+++ hmeworke/src/components/lol.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <form
+        :style="{ display: 'flex', flexDirection: 'column', width: '200px'}">
+        <button>lol_ehehehe</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
